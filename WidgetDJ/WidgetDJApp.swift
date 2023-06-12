b315e32c4df15c59da7619e4afcc8abc671c5bf4
John Baer <johnbaer3@gmail.com>
2023-06-11 18:18:28 -0700

    Initial Commit

---
+++ WidgetDJ/WidgetDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDJApp.swift
+//  WidgetDJ
+//
+//  Created by John Baer on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
